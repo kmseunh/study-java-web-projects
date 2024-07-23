b242373708aa4568e519fe382b635761624c3214
kmseunh <tmdgus8779@gmail.com>
2024-07-23 23:47:32 +0900

    feat: 회원 정보를 처리하고 JSP 페이지로 전달하는 서블릿 추가
    - `MemberDAO`를 사용하여 회원 정보 조회
    - HTTP GET 및 POST 요청 모두 `doHandle` 메서드로 처리
    - 조회된 회원 정보를 `memberList` 속성으로 설정
    - `/listMembers.jsp`로 요청 포워딩

--- MemberAdmin/MemberAdminServlet/src/main/java/com/example/project/controller/MemberController.java
+++ MemberAdmin/MemberAdminServlet/src/main/java/com/example/project/controller/MemberController.java
@@ -1,5 +1,50 @@
 package com.example.project.controller;
 
-public class MemberController {
 
+import com.example.project.dao.MemberDAO;
+import com.example.project.vo.MemberVO;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/mem.do")
+public class MemberController extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    MemberDAO memberDAO;
+
+    @Override
+    public void init() throws ServletException {
+        memberDAO = new MemberDAO();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException {
+        doHandle(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException {
+        doHandle(req, resp);
+    }
+
+    private void doHandle(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html;charset=utf-8");
+
+        List<MemberVO> memberList = memberDAO.listMembers();
+        req.setAttribute("memberList", memberList);
+
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/listMembers.jsp");
+        dispatcher.forward(req, resp);
+    }
 }
