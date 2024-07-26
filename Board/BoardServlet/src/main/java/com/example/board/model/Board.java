d340322abd40c37afcb59bcdda8e5e095e0f1037
kmseunh <tmdgus8779@gmail.com>
2024-07-27 01:18:05 +0900

    feat: Board 클래스 생성
    - Board 클래스 생성
    - 멤버 변수: num, title, writer, content, cnt, regdate, lastModified 추가
    - 기본 생성자 및 다양한 매개변수를 받는 생성자 추가
    - getter/setter 메서드 추가

--- Board/BoardServlet/src/main/java/com/example/board/model/Board.java
+++ Board/BoardServlet/src/main/java/com/example/board/model/Board.java
@@ -1,5 +1,98 @@
 package com.example.board.model;
 
+import java.util.Date;
+
 public class Board {
 
+    private int num;
+    private String title;
+    private String writer;
+    private String content;
+    private int cnt;
+    private Date regdate;
+    private Date lastModified;
+
+    public Board() {
+
+    }
+
+    public Board(String title, String writer, String content) {
+        this.title = title;
+        this.writer = writer;
+        this.content = content;
+    }
+
+    public Board(int num, String title, String writer, String content) {
+        this.num = num;
+        this.title = title;
+        this.writer = writer;
+        this.content = content;
+    }
+
+    public Board(int num, String title, String writer, String content, int cnt, Date regdate,
+        Date lastModified) {
+        this.num = num;
+        this.title = title;
+        this.writer = writer;
+        this.content = content;
+        this.cnt = cnt;
+        this.regdate = regdate;
+        this.lastModified = lastModified;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getCnt() {
+        return cnt;
+    }
+
+    public void setCnt(int cnt) {
+        this.cnt = cnt;
+    }
+
+    public Date getRegdate() {
+        return regdate;
+    }
+
+    public void setRegdate(Date regdate) {
+        this.regdate = regdate;
+    }
+
+    public Date getLastModified() {
+        return lastModified;
+    }
+
+    public void setLastModified(Date lastModified) {
+        this.lastModified = lastModified;
+    }
 }
