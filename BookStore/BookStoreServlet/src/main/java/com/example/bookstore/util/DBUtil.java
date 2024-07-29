0e9665b25fb5861918f2fa4b2724fc8e05725752
kmseunh <tmdgus8779@gmail.com>
2024-07-29 13:16:13 +0900

    feat: DBUtil 클래스 초기 구현
    - HikariCP를 사용한 데이터베이스 연결 풀 설정
    - MariaDB JDBC 드라이버 로드
    - 데이터 소스 초기화 및 설정 (JDBC URL, 사용자명, 비밀번호, 캐시 설정 등)
    - getConnection 메서드를 통해 데이터베이스 연결 반환
    - 예외 처리 및 로깅 추가

--- BookStore/BookStoreServlet/src/main/java/com/example/bookstore/util/DBUtil.java
+++ BookStore/BookStoreServlet/src/main/java/com/example/bookstore/util/DBUtil.java
@@ -1,5 +1,44 @@
 package com.example.bookstore.util;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class DBUtil {
 
+    private static final Logger LOGGER = Logger.getLogger(DBUtil.class.getName());
+    private static HikariDataSource dataSource;
+
+    static {
+        try {
+            initializeDataSource();
+        } catch (ClassNotFoundException e) {
+            LOGGER.log(Level.SEVERE, "Error in initializeDataSource", e);
+        }
+    }
+
+    public static void initializeDataSource() throws ClassNotFoundException {
+        Class.forName("org.mariadb.jdbc.Driver");
+
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:mariadb://localhost:3306/study");
+        config.setUsername("kmseunh");
+        config.setPassword("1111");
+
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+
+        dataSource = new HikariDataSource(config);
+    }
+
+    public static Connection getConnection() throws SQLException {
+        if (dataSource == null) {
+            throw new SQLException("DataSouce is not initialized");
+        }
+        return dataSource.getConnection();
+    }
 }
