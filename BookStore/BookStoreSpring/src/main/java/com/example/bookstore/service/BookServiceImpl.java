01634551567ff6912335d82a4350892b41aba900
kmseunh <tmdgus8779@gmail.com>
2024-07-31 11:53:20 +0900

    feat: BookServiceImpl에 create 메서드 추가
    - 책 정보를 삽입하고, 삽입된 책의 ID를 반환하는 create 메서드 추가

--- BookStore/BookStoreSpring/src/main/java/com/example/bookstore/service/BookServiceImpl.java
+++ BookStore/BookStoreSpring/src/main/java/com/example/bookstore/service/BookServiceImpl.java
@@ -1,5 +1,22 @@
 package com.example.bookstore.service;
 
-public class BookServiceImpl {
+import com.example.bookstore.dao.BookDAO;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
+public class BookServiceImpl implements BookService {
+
+    @Autowired
+    BookDAO bookDAO;
+
+    @Override
+    public String create(Map<String, Object> map) {
+        int affectRowCount = this.bookDAO.insert(map);
+        if (affectRowCount == 1) {
+            return map.get("book_id").toString();
+        }
+        return null;
+    }
 }
