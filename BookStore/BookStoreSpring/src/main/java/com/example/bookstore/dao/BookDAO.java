5f3bb2a9d5d7a5f084e2ae5cd0ce14f84b279296
kmseunh <tmdgus8779@gmail.com>
2024-07-31 20:12:54 +0900

    feat: insert 메서드 구현
    - MyBatis를 사용하여 데이터베이스에 책 정보를 삽입하는 기능 추가
    - insert 메서드에서 SqlSessionTemplate을 이용하여 "book.insert" 매퍼 호출

--- BookStore/BookStoreSpring/src/main/java/com/example/bookstore/dao/BookDAO.java
+++ BookStore/BookStoreSpring/src/main/java/com/example/bookstore/dao/BookDAO.java
@@ -1,5 +1,17 @@
 package com.example.bookstore.dao;
 
+import java.util.Map;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class BookDAO {
 
+    @Autowired
+    SqlSessionTemplate sqlSessionTemplate;
+
+    public int insert(Map<String, Object> map) {
+        return this.sqlSessionTemplate.insert("book.insert", map);
+    }
 }
