077607e1fec9dee0e53da8fb3e115d8206117706
kmseunh <tmdgus8779@gmail.com>
2024-07-31 12:14:15 +0900

    feat: 책 생성 기능 추가
    - GET /create: 책 생성 페이지 반환
    - POST /create: 책 정보를 수집하여 책을 생성하고, 성공 시 상세 페이지로 리다이렉션

--- BookStore/BookStoreSpring/src/main/java/com/example/bookstore/controller/BookController.java
+++ BookStore/BookStoreSpring/src/main/java/com/example/bookstore/controller/BookController.java
@@ -1,5 +1,35 @@
 package com.example.bookstore.controller;
 
+import com.example.bookstore.service.BookService;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class BookController {
 
+    @Autowired
+    BookService bookService;
+
+    @RequestMapping(value = "/create", method = RequestMethod.GET)
+    public ModelAndView create() {
+        return new ModelAndView("book/createBook");
+    }
+
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public ModelAndView createPost(@RequestParam Map<String, Object> map) {
+        ModelAndView mav = new ModelAndView();
+
+        String bookId = this.bookService.create(map);
+        if (bookId == null) {
+            mav.setViewName("redirect:/create");
+        } else {
+            mav.setViewName("redirect:/detail?bookId=" + bookId);
+        }
+        return mav;
+    }
 }
